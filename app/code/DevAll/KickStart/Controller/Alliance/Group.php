88abcecc3502ef6c4e2400033840ec2bd9729505
giorgitsignadze <giorgi.tsignadze.1@btu.edu.ge>
2025-03-07 19:10:36 +0400

    first commit

---
+++ app/code/DevAll/KickStart/Controller/Alliance/Group.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Developers-Alliance team
+ * @copyright Copyright (c) 2024 Developers-alliance (https://www.developers-alliance.com)
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace DevAll\KickStart\Controller\Alliance;
+
+use JetBrains\PhpStorm\NoReturn;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Group implements HttpGetActionInterface
+{
+    #[NoReturn] public function execute()
+    {
+        die('It is done');
+    }
+}
