c2621930d77e6c2a438368054b9640fec1e8490d
giorgitsignadze <giorgi.tsignadze.1@btu.edu.ge>
2025-03-09 20:59:12 +0400

    redirect

---
+++ app/code/DevAll/KickStart/Controller/Alliance/GroupTemplate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DevAll\KickStart\Controller\Alliance;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NotFoundException;
+
+class GroupTemplate implements HttpGetActionInterface
+{
+    /**
+     * Execute action based on request and return result
+     *
+     * @return ResultInterface|ResponseInterface
+     * @throws NotFoundException
+     */
+    public function execute()
+    {
+        // TODO: Implement execute method.
+    }
+}
