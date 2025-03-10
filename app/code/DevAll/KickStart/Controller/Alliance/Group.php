3f8bcaa7dd2ebcd8ed86fdc3ea1e7a75ad844124
giorgitsignadze <giorgi.tsignadze.1@btu.edu.ge>
2025-03-10 14:58:25 +0400

    update controllers

--- app/code/DevAll/KickStart/Controller/Alliance/Group.php
+++ app/code/DevAll/KickStart/Controller/Alliance/Group.php
@@ -11,11 +11,22 @@ namespace DevAll\KickStart\Controller\Alliance;
 
 use JetBrains\PhpStorm\NoReturn;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Psr\Log\LoggerInterface;
 
 class Group implements HttpGetActionInterface
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly RequestInterface $request,
+    ) {
+    }
+
     #[NoReturn] public function execute(): void
     {
-        die('It is done');
+        $this->logger->info('Alliance Group Controller');
+        echo '<pre>';
+        var_dump($this->request->getParams());
+        die();
     }
 }
